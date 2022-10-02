c3c37672d3db74ab31a39dd15b5820b29538c0bc
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2022-10-02 20:39:02 +0200

    Configure logging

--- investd/__main__.py
+++ investd/__main__.py
@@ -1,11 +1,16 @@
+import logging
 import sys
 
 from .source import load_txs_from_source
 
+log = logging.getLogger("investd")
+
 
 def save_tx():
     df_tx = load_txs_from_source()
-    df_tx.to_csv("data/transactions/tx.csv", index=False)
+    output = "data/transactions/tx.csv"
+    log.info(f"Writing {output}")
+    df_tx.to_csv(output, index=False)
 
 
 if __name__ == "__main__":
@@ -14,3 +19,4 @@ if __name__ == "__main__":
         save_tx()
     else:
         print(f"Unrecognized command {command}")
+    log.info("Finish.")
--- investd/source/base.py
+++ investd/source/base.py
@@ -1,8 +1,11 @@
 import abc
+import logging
 from pathlib import Path
 from typing import Generator, Iterable
 
 from ..model import Transaction
+
+log = logging.getLogger(__name__)
 
 
 class SourceBase(metaclass=abc.ABCMeta):
@@ -18,5 +21,6 @@ class SourceBase(metaclass=abc.ABCMeta):
 
     def load_transactions(self) -> Generator[Transaction, None, None]:
         for path in Path("data/source/" + self.source_name).glob("*"):
+            log.info(f"Loading {path}")
             for tx in self.parse_source_file(path):
                 yield tx
