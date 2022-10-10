1a99030fffbd9ba6e1dbe9bf53ef59add8573b61
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2022-10-10 22:03:44 +0200

    Fix loading .test.env config file during tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,12 +4,17 @@ import pytest
 
 
 @pytest.fixture
-def cleanup_reports():
+def cleanup_reports(path_resources):
     yield
-    for path in Path("sample_data/reports").glob("*.html"):
+    for path in (path_resources / "data/reports").glob("*.html"):
         path.unlink()
 
 
 @pytest.fixture
-def path_revolut_csv() -> Path:
-    return Path("tests/resources/data/sources/revolut-stocks-statement.csv")
+def path_revolut_csv(path_resources) -> Path:
+    return path_resources / "data/sources/revolut-stocks-statement.csv"
+
+
+@pytest.fixture
+def path_resources() -> Path:
+    return Path(__file__).parent / "resources"
