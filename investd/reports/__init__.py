cd8408a59dfc2a083b01ee20ce900f932aee0f2c
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2022-10-12 19:35:24 +0200

    Refactor reports.__init__

--- investd/reports/__init__.py
+++ investd/reports/__init__.py
@@ -1,23 +1,23 @@
-from importlib import import_module
+from datetime import datetime
 from pathlib import Path
 
 import jupytext
 from nbconvert.exporters import HTMLExporter, export
 from nbconvert.preprocessors import ExecutePreprocessor
 
-from ..config import REPORTS_PATH
+from ..config import REF_CURRENCY, REPORTS_PATH
 
 
-def generate_report(notebook_name: str) -> None:
-    nb_module = import_module(f"{__package__}.{notebook_name}")
-    notebook = jupytext.read(nb_module.__file__)
-    nb_preprocessor = ExecutePreprocessor(timeout=10, kernel_name="python3")
-    notebook_executed, _ = nb_preprocessor.preprocess(
+def generate_report(notebook_name: str) -> Path:
+    path_notebook = Path(__file__).parent / f"{notebook_name}.py"
+    notebook = jupytext.read(path_notebook)
+    preprocessor = ExecutePreprocessor(timeout=10, kernel_name="python3")
+    nb_executed, _ = preprocessor.preprocess(
         notebook, {"metadata": {"path": str(Path.cwd())}}
     )
     output, _ = export(
         HTMLExporter,
-        notebook_executed,
+        nb_executed,
         config={
             "TemplateExporter": {
                 "exclude_output_prompt": True,
@@ -26,5 +26,14 @@ def generate_report(notebook_name: str) -> None:
             }
         },
     )
-    with Path(REPORTS_PATH / f"{notebook_name}.html").open("w") as out_file:
-        out_file.write(output)
+    report_path = _save_report(name=notebook_name, content=output)
+    return report_path
+
+
+def _save_report(name: str, content: str) -> Path:
+    today = datetime.today().strftime("%Y-%m-%d")
+    filename = f"{name}_{today}_{str(REF_CURRENCY)}.html"
+    report_path = REPORTS_PATH / filename
+    with report_path.open("w") as out_file:
+        out_file.write(content)
+    return report_path
