41f3484719b358e0a45a7e1a9270736364e1a8ad
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2022-10-12 19:36:33 +0200

    Refactor reports.__init__ (#21)

--- tests/test_reports.py
+++ tests/test_reports.py
@@ -1,8 +1,8 @@
+from investd.config import REPORTS_PATH
 from investd.reports import generate_report
 
 
-def test_generate_overview_report(cleanup_reports, path_resources):
-    generated_report_path = path_resources / "data/reports/overview.html"
-    assert not generated_report_path.exists()
-    generate_report("overview")
-    assert generated_report_path.exists()
+def test_generate_overview_report(cleanup_reports):
+    assert not list(REPORTS_PATH.glob("*.html"))
+    report_path = generate_report("overview")
+    assert report_path in list(REPORTS_PATH.glob("*.html"))
