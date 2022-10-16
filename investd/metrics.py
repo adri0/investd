a13f678bc2740293a0b92bbc9a387ee0f05292f9
Adriano Oliveira <8104069+adri0@users.noreply.github.com>
2022-10-16 14:42:06 +0200

    Add invested_amount_original_currency_by_col function in metrics.py. And Tests.

--- investd/metrics.py
+++ investd/metrics.py
@@ -9,18 +9,21 @@ import pandas as pd
 from .model import Action
 
 
-def signed_ref_amount(df_tx: pd.DataFrame) -> pd.DataFrame:
+def add_signed_cols(df_tx: pd.DataFrame) -> pd.DataFrame:
     """
-    Add 'signed_ref_amount' col to dataframe corresponding to
-    negative sign to amount_ref_currency when action SELL.
+    Add columns ending with _signed to dataframe corresponding to
+    negative sign to original amount column when action SELL.
     """
-    if "signed_ref_amount" in df_tx.columns:
-        return df_tx
+    cols = ("amount_ref_currency", "amount")
 
-    def get_signed_amount(row) -> float:
-        return row["amount_ref_currency"] * (-1 if row["action"] == Action.SELL else 1)
+    def get_signed_amount(row, col: str) -> float:
+        return row[col] * (-1 if row["action"] == Action.SELL else 1)
 
-    df_tx["signed_ref_amount"] = df_tx.apply(get_signed_amount, axis=1)
+    for col in cols:
+        signed_col_name = col + "_signed"
+        if signed_col_name in df_tx.columns:
+            continue
+        df_tx[signed_col_name] = df_tx.apply(get_signed_amount, axis=1, args=(col,))
     return df_tx
 
 
@@ -28,13 +31,24 @@ def total_invested_ref_currency(df_tx: pd.DataFrame) -> float:
     """
     Calculates total invested amount in the reference currency.
     """
-    df_tx = signed_ref_amount(df_tx)
-    return df_tx["signed_ref_amount"].sum()
+    df_tx = add_signed_cols(df_tx)
+    return df_tx["amount_ref_currency_signed"].sum()
 
 
 def invested_ref_amount_by_col(df_tx: pd.DataFrame, col: str) -> pd.Series:
     """
     Generates a Series with invested amount by asset type in the reference currency.
     """
-    df_tx = signed_ref_amount(df_tx)
-    return df_tx.groupby(col)["signed_ref_amount"].sum()
+    df_tx = add_signed_cols(df_tx)
+    return df_tx.groupby(col)["amount_ref_currency_signed"].sum()
+
+
+def invested_amount_original_currency_by_col(
+    df_tx: pd.DataFrame, col: str
+) -> pd.core.generic.NDFrame:
+    """
+    Agregate invested amount by currency.
+    """
+    df_tx = add_signed_cols(df_tx)
+    grouping = ["currency", col] if col != "currency" else col
+    return df_tx.groupby(grouping)["amount_signed"].sum()
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,16 +1,20 @@
 import pandas as pd
 import pytest
 
-from investd.metrics import invested_ref_amount_by_col, total_invested_ref_currency
+from investd.metrics import (
+    invested_amount_original_currency_by_col,
+    invested_ref_amount_by_col,
+    total_invested_ref_currency,
+)
 from investd.model import AssetType, Currency
 
 
-def test_invested_amount(df_tx_minimal: pd.DataFrame):
+def test_total_invested_amount(df_tx_minimal: pd.DataFrame):
     assert total_invested_ref_currency(df_tx_minimal) == 2349.0
 
 
 @pytest.mark.parametrize(
-    "col,expected_result",
+    "col,expected",
     [
         ("type", pd.Series({AssetType.Stock: 849, AssetType.ETF: 1500})),
         ("platform", pd.Series({"revolut_stocks": 849, "xtb": 1500})),
@@ -19,6 +23,41 @@ def test_invested_amount(df_tx_minimal: pd.DataFrame):
     ],
 )
 def test_invested_ref_amount_col(
-    df_tx_minimal: pd.DataFrame, col: str, expected_result: pd.core.generic.NDFrame
+    df_tx_minimal: pd.DataFrame, col: str, expected: pd.Series
 ):
-    assert invested_ref_amount_by_col(df_tx_minimal, col).equals(expected_result)
+    assert invested_ref_amount_by_col(df_tx_minimal, col).equals(expected)
+
+
+@pytest.mark.parametrize(
+    "col,expected",
+    [
+        ("currency", pd.Series({Currency.USD: 190.0, Currency.EUR: 300.0})),
+        (
+            "type",
+            pd.Series(
+                {
+                    (Currency.USD, AssetType.Stock): 190.0,
+                    (Currency.USD, AssetType.ETF): 0.0,
+                    (Currency.EUR, AssetType.Stock): 0.0,
+                    (Currency.EUR, AssetType.ETF): 300.0,
+                }
+            ),
+        ),
+        (
+            "platform",
+            pd.Series(
+                {
+                    (Currency.USD, "revolut_stocks"): 190.0,
+                    (Currency.USD, "xtb"): 0.0,
+                    (Currency.EUR, "revolut_stocks"): 0.0,
+                    (Currency.EUR, "xtb"): 300.0,
+                }
+            ),
+        ),
+    ],
+)
+def test_invested_amount_currency_amount_col(
+    df_tx_minimal: pd.DataFrame, col: str, expected: pd.core.generic.NDFrame
+):
+    ndf = invested_amount_original_currency_by_col(df_tx_minimal, col)
+    assert expected.equals(ndf)
